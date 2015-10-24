0b3ae14578caf9a224e614109f56477ea07b5c94
Jason Pincin <jason@pincin.com>
2015-10-24 12:50:42 -0400

    no need for default

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var inherits          = require('util').inherits
+var inherits = require('util').inherits
 
 module.exports = function defineError (name, init) {
     name = typeof name === 'string' ? name : 'UndefinedError'
@@ -6,7 +6,7 @@ module.exports = function defineError (name, init) {
 
     function ErrorCtor (message) {
         if (Error.captureStackTrace) Error.captureStackTrace(this, ErrorCtor)
-        else this.stack = (new Error).stack || ''
+        else this.stack = (new Error).stack
         this.message = message
         init.apply(this, arguments)
     }
