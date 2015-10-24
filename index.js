08252d367217ca6bf1c61c85aa34220ffecd587f
Jason Pincin <jason@pincin.com>
2015-10-24 12:34:18 -0400

    capture stack trace in more compatible way

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var inherits          = require('util').inherits,
-    captureStackTrace = require('capture-stack-trace')
+var inherits          = require('util').inherits
 
 module.exports = function defineError (name, init) {
     name = typeof name === 'string' ? name : 'UndefinedError'
     init = typeof init === 'function' ? init : function () {}
 
     function ErrorCtor (message) {
-        captureStackTrace(this, ErrorCtor)
+        if (Error.captureStackTrace) Error.captureStackTrace(this, ErrorCtor)
+        else this.stack = (new Error).stack || ''
         this.message = message
         init.apply(this, arguments)
     }
