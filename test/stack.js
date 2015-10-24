c0ed55eed8630da664a3c9250e71f76aae0669a1
Jason Pincin <jason@pincin.com>
2015-10-24 12:50:55 -0400

    test more conditions

--- test/stack.js
+++ test/stack.js
@@ -9,3 +9,17 @@ test('errors should have stack property', function (t) {
     // not all browsers give us a stack, always pass this, but log whether we have it or not
     t.pass('stack property ' + (err.stack ? 'does exists' : 'does NOT exist'))
 })
+
+test('errors should have stack property (without Error.captureStackTrace)', function (t) {
+    t.plan(1)
+
+    var _captureStackTrace = Error.captureStackTrace
+    delete Error.captureStackTrace
+
+    var MyError = error('MyError')
+    var err = new MyError('uh oh')
+    // not all browsers give us a stack, always pass this, but log whether we have it or not
+    t.pass('stack property ' + (err.stack ? 'does exists' : 'does NOT exist'))
+
+    Error.captureStackTrace = _captureStackTrace
+})
